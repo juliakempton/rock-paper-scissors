3a2e07d0658b7539694eb214f34de25752f2b7de
Julia Kempton <kemptonjq@g.cofc.com>
2021-06-21 11:42:57 -0400

    Created function that plays one round of rock paper scissors

--- game-play.js
+++ game-play.js
@@ -1,7 +1,35 @@
-function computerPlay () {
-    let selections = ["Rock", "Paper", "Scissors"];
+function computerPlay() {
+    let selections = ["rock", "paper", "scissors"];
     let randomSelection = selections[Math.floor(Math.random() * selections.length)];
     return randomSelection;
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLower = playerSelection.toLowerCase();
+    if (playerSelectionLower === computerSelection) {
+        return "It's a tie!"
+    } 
+    if (playerSelectionLower === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors";
+        }
+    } else if (playerSelectionLower === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock";
+        } else {
+            return "You lose! Scissors beats Paper";
+        }
+    } else if (playerSelectionLower === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors";
+        } else {
+            return "You win! Scissors beats Paper";
+        }
+    }
+}
+
+const playerSelection = window.prompt("Enter your selection: ");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
